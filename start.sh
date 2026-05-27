f9ecdba5c517b8eb97c754f7733551afe02861c1
Syleinfinity <khanhsky2k5nam@gmail.com>
2026-05-27 19:38:59 +0700

    batch 39_p1: 7909 files

---
+++ start.sh
@@ -0,0 +1,4 @@
+# #!/bin/bash
+# java -Xmx6G -Xms4G -jar forge-1.12.2-14.23.5.2859.jar nogui
+#java -Xmx6G -Xms4G -jar forge-1.12.2-14.23.5.2860.jar nogui
+/opt/java8/bin/java -Xms10G -Xmx10G -jar forge-1.12.2-14.23.5.2860.jar nogui
