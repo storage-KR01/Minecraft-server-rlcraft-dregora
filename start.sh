d27147c55f77a18c4034a6ccde925393b332f687
storage-KR01 <sylein111@gmail.com>
2026-05-28 09:17:22 +0000

    16h18p 28/05/2026

--- start.sh
+++ start.sh
@@ -1,4 +1,2 @@
-# #!/bin/bash
-# java -Xmx6G -Xms4G -jar forge-1.12.2-14.23.5.2859.jar nogui
-#java -Xmx6G -Xms4G -jar forge-1.12.2-14.23.5.2860.jar nogui
+
 /opt/java8/bin/java -Xms10G -Xmx10G -jar forge-1.12.2-14.23.5.2860.jar nogui
